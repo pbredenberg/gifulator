822a9407e79d6c3f65e112dadc29fa38d88e1b63
Paul Bredenberg <paulbredenberg@gmail.com>
2021-07-31 07:48:18 -0400

    fix: add new types definition since next.js now overwrites next-env

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,10 +1,3 @@
 /// <reference types="next" />
 /// <reference types="next/types/global" />
-
-declare module '*.md' {
-  import React from 'react';
-  const react: React.VFC;
-  // Intentionally unknown so types can be cast.
-  const attributes: unknown;
-  export { attributes, react };
-}
+/// <reference types="next/image-types/global" />
---
+++ type.d.ts
@@ -0,0 +1,7 @@
+declare module '*.md' {
+  import React from 'react';
+  const react: React.VFC;
+  // Intentionally unknown so types can be cast.
+  const attributes: unknown;
+  export { attributes, react };
+}
