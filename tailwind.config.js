ce897f1018efdd4a22f13b132da5e2ac3c4d8ad3
Paul Bredenberg <paulbredenberg@gmail.com>
2021-08-01 09:52:38 -0400

    chore: add tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [
+    require( 'tailwindcss' ),
+		require( 'precss' ),
+		require( 'autoprefixer' )
+  ],
+}
